c85dd9c3a9caea0eb6b2135b3fde1ea04a5df7a4
Julia <kozlova_yulia2@mail.ru>
2020-08-03 18:17:29 +0300

    Internet connection was checked

--- weather_application/lib/main.dart
+++ weather_application/lib/main.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
+
+import 'package:connectivity/connectivity.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:connectivity/connectivity.dart';
-
 
 void main() {
   runApp(
@@ -24,7 +26,7 @@ class Home extends StatefulWidget{
   }
 }
 
-/// class description for the next 5 days every 3 hours
+/// Class description for the next 5 days every 3 hours.
 class WeatherInfo {
   var description;
   var temp;
@@ -63,19 +65,10 @@ class HomeState extends State<Home> {
   var pressure;
   var windDirection;
   var _curIndex = 0;
-
-  /*bool checkConnection(){
-    var connectivityResult = Connectivity().checkConnectivity();
-    if ((connectivityResult == ConnectivityResult.mobile) || (connectivityResult == ConnectivityResult.wifi)) {
-      return true;
-    } else return false;
-  }
-
-  bool isInternetConnected;*/
-
+  bool isInternetConnected = false;
   List<WeatherInfo> weatherInfo = [];
 
-  /// getting information for today from API
+  /// Getting information for today from API.
   Future getWeather () async {
     http.Response response = await http.get("http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=4f8c59216c41ae9c18d1af6ddc81a0c6");
     var result = jsonDecode(response.body);
@@ -91,18 +84,19 @@ class HomeState extends State<Home> {
     setWindDirection();
   }
 
-  /// getting information for the next 5 days from API
+  /// Getting information for the next 5 days from API.
   Future getWeatherFor5Days () async {
     http.Response response = await http.get("http://api.openweathermap.org/data/2.5/forecast?q=Minsk&appid=4f8c59216c41ae9c18d1af6ddc81a0c6");
     var result = jsonDecode(response.body);
 
     for (var number = 0;  number < 40; number++) {
-      WeatherInfo info = WeatherInfo(result['list'][number]['weather'][0]['description'], result['list'][number]['main']['temp'], result['list'][number]['dt_txt']);
+      WeatherInfo info = WeatherInfo(result['list'][number]['weather'][0]['description'],
+          result['list'][number]['main']['temp'], result['list'][number]['dt_txt']);
       weatherInfo.add(info);
     }
   }
 
-  /// define wind direction according to the degree
+  /// Define wind direction according to the degree.
   void setWindDirection(){
     if ((windDirection <= 10) || (windDirection >= 350)) {windDirection = 'N';}
     else if ((windDirection > 10) && (windDirection < 80)) {windDirection = 'NE';}
@@ -114,14 +108,12 @@ class HomeState extends State<Home> {
     else if ((windDirection > 280) && (windDirection < 350)) {windDirection = 'NW';}
   }
 
-
   @override
   void initState () {
     super.initState();
     this.getWeather();
     this.getWeatherFor5Days();
   }
-
 
   Widget _bottomNormal()=> BottomNavigationBar(
     backgroundColor: Colors.white,
@@ -160,141 +152,183 @@ class HomeState extends State<Home> {
   @override
   Widget build (BuildContext context) {
 
-    if (_curIndex == 0 ) {
-
+    if (isInternetConnected) {
+      //_getCurrentLocation();
       return Scaffold(
           appBar: AppBar(
             centerTitle: true,
-            title: const Text('Minsk', style: TextStyle(
+            title: const Text('', style: TextStyle(
                 color: Colors.black
             )),
             backgroundColor: Colors.white,
           ),
-          body:
-          Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 10.0),
-                      child: Text(
-                        "Currently in Minsk",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w600
-                        ),
-                      ),
-                    ),
-                    Text(
-                        temp != null ? (temp - 273.15).round().toString() + "\u00B0" : "Loading",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.w600
-                        )
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                        child: Text(
-                            currently != null ? currently.toString() : "Loading",
+      );
+    }
+    else {
+      if (_curIndex == 0) {
+        return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('Minsk', style: TextStyle(
+                  color: Colors.black
+              )),
+              backgroundColor: Colors.white,
+            ),
+            body:
+            Column(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height / 3,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  color: Colors.white,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10.0),
+                          child: Text(
+                            "Currently in Minsk",
                             style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 14.0,
                                 fontWeight: FontWeight.w600
+                            ),
+                          ),
+                        ),
+                        Text(
+                            temp != null ? (temp - 273.15).round().toString() +
+                                "\u00B0" : "Loading",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w600
+                            )
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: Text(
+                                currently != null
+                                    ? currently.toString()
+                                    : "Loading",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w600
+                                )
                             )
                         )
-                    )
-                  ],
+                      ]
+                  ),
                 ),
-              ),
 
-              Expanded(
-                  //color: Colors.white,
-
-                  child: Padding(
-                      padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
-                      child: Column(
-                          children: [
-                            Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-
+                Expanded(
+                    child: Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
+                        child: Column(
                             children: [
-                              _buildButtonColumn(Colors.orangeAccent, MdiIcons.weatherRainy, humidity != null ? humidity.toString() + '%' : "Loading" ),
-                              _buildButtonColumn(Colors.orangeAccent, MdiIcons.water, '?'),
-                              _buildButtonColumn(Colors.orangeAccent, MdiIcons.temperatureCelsius, pressure != null ? pressure.toString() + ' hPh' : "Loading"),
-                            ],
-                            ),
-                            Spacer(),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                _buildButtonColumn(Colors.orangeAccent, MdiIcons.weatherWindy, windSpeed != null ? (windSpeed * 3.6).toString() + ' km/h' : "Loading"),
-                                _buildButtonColumn(Colors.orangeAccent, MdiIcons.compass, windDirection != null ? windDirection.toString() : "Loading"),
-                              ],
-                            ),
-                            Spacer(),
-                          ]
-                      )
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment
+                                    .spaceAround,
 
-                      )
-                  )
-
-            ],
-          ),
-          bottomNavigationBar: _bottomNormal()
-      );
-    }
-    else return Scaffold(
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20.0, bottom: 20.0, left: 16.0, right: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new ListTile(
-                      leading: FaIcon(FontAwesomeIcons.wind),
-                      title: Text(weatherInfo != null ? weatherInfo[index].time.toString().substring(11, 16) : "Loading",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black
-                        ),
-                      ),
-                      subtitle: Text(weatherInfo != null ? weatherInfo[index].description : "Loading",
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.normal
-                            //color: Colors.black
-                        ),
-                      ),
-                      trailing: Text(
-                          weatherInfo[index].temp != null ?
-                          (weatherInfo[index].temp - 273.15).round().toString() + "\u00B0" :
-                          "Loading",
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.blue
-                          ),
-                      ),
+                                children: [
+                                  _buildButtonColumn(Colors.orangeAccent,
+                                      MdiIcons.weatherRainy,
+                                      humidity != null ? humidity.toString() +
+                                          '%' : "Loading"),
+                                  _buildButtonColumn(
+                                      Colors.orangeAccent, MdiIcons.water, '?'),
+                                  _buildButtonColumn(Colors.orangeAccent,
+                                      MdiIcons.temperatureCelsius,
+                                      pressure != null ? pressure.toString() +
+                                          ' hPh' : "Loading"),
+                                ],
+                              ),
+                              Spacer(),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment
+                                    .spaceEvenly,
+                                children: [
+                                  _buildButtonColumn(Colors.orangeAccent,
+                                      MdiIcons.weatherWindy,
+                                      windSpeed != null ? (windSpeed * 3.6)
+                                          .toString() + ' km/h' : "Loading"),
+                                  _buildButtonColumn(
+                                      Colors.orangeAccent, MdiIcons.compass,
+                                      windDirection != null ? windDirection
+                                          .toString() : "Loading"),
+                                ],
+                              ),
+                              Spacer(),
+                            ]
+                        )
                     )
-                  ],
-                )
-              )
-            );
-          },
-          itemCount: weatherInfo.length,
-        ),
-        bottomNavigationBar: _bottomNormal()
-    );
+                ),
+              ],
+            ),
+            bottomNavigationBar: _bottomNormal()
+        );
+      }
+      else
+        return Scaffold(
+            body: ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20.0, bottom: 20.0, left: 16.0, right: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            new ListTile(
+                              leading: FaIcon(FontAwesomeIcons.wind),
+                              title: Text(
+                                weatherInfo != null ? weatherInfo[index].time
+                                    .toString().substring(11, 16) : "Loading",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.black
+                                ),
+                              ),
+                              subtitle: Text(
+                                weatherInfo != null ? weatherInfo[index]
+                                    .description : "Loading",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.normal
+                                  //color: Colors.black
+                                ),
+                              ),
+                              trailing: Text(
+                                weatherInfo[index].temp != null ?
+                                (weatherInfo[index].temp - 273.15)
+                                    .round()
+                                    .toString() + "\u00B0" :
+                                "Loading",
+                                style: TextStyle(
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.blue
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    )
+                );
+              },
+              itemCount: weatherInfo.length,
+            ),
+            bottomNavigationBar: _bottomNormal()
+        );
+    }
   }
 }
